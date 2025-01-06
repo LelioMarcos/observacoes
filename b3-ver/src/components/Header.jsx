74f7d77cc229025a8f81ae06c0c26d09f12dfab6
LelioMarcos <leliomrcunha@gmail.com>
2025-01-05 22:28:20 -0300

    Logout

--- b3-ver/src/components/Header.jsx
+++ b3-ver/src/components/Header.jsx
@@ -4,15 +4,31 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../providers/AuthProvider';
 
 function Header() {
-    const { email } = useAuth(); 
+    const { email, clearAuth } = useAuth(); 
+
+    const logOut = () => {
+        clearAuth();
+    }
 
     return (
         <>
             <Group justify="flex-end" m={{ xs: 'lg', sm: "md" }} p="10px">
-                {email && <Text>{email}</Text>}
-                <Link to="/login">
-                    <Button>Login</Button>
-                </Link>
+                {email ? 
+                    <Text>{email}</Text>
+                    :
+                    <Link to="/register">
+                        <Button>Register</Button>
+                    </Link>
+                }
+                {email ? 
+                    <Link onClick={logOut}>
+                        <Button>Logout</Button>
+                    </Link>
+                    :
+                    <Link to="/login">
+                        <Button>Login</Button>
+                    </Link>
+                }
             </Group>
         </>
     )
